9340fc00e25faaea8c94cd25b69113bcd815ee8c
Don Puerto <don.puerto@hotmail.com>
2026-01-29 21:34:29 +0800

    .

--- components/dashboard/language-switcher.tsx
+++ components/dashboard/language-switcher.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import * as React from "react"
+
 import { Globe } from "lucide-react"
 
 import { Button } from "@/components/ui/button"
--- components/dashboard/theme-toggle.tsx
+++ components/dashboard/theme-toggle.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import * as React from "react"
+
 import { Moon, Sun } from "lucide-react"
 import { useTheme } from "@/app/hooks/useTheme"
 import { Button } from "@/components/ui/button"
